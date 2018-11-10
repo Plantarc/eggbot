b35ed19b241751f85bff8d3dc32db4a044bb5992
Playermode <36794669+Playermode@users.noreply.github.com>
2018-11-09 22:45:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1054,7 +1054,7 @@ client.on("message", function(message) {
 });
 
 client.on('ready', function() { 
-    client.user.setGame("Scaring the egg out of eggmode");
+    client.user.setGame("Egg or Egg");
     client.user.setUsername("Egg Bot");
     console.log('I am ready!');
 });
