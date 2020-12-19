126678bb5a41aff2cd52f02de3457339a247fc4a
Playermode <36794669+Playermode@users.noreply.github.com>
2020-12-19 18:08:04 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1604,6 +1604,12 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
+    if (message.content === "poop") {
+    	message.reply('HOW https://cdn.discordapp.com/attachments/313415730496274441/789708100969431060/funny.png');
+    }    
+});
+
+client.on('message', message => {
     if (message.content === "joe mama") {
     	message.reply('NO NOT MY ONLY WEAKNESS');
     }    
