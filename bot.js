120b63b94a09ba3e7020e9814e6efe20f8e822d5
Playermode <36794669+Playermode@users.noreply.github.com>
2022-02-10 20:30:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1350,7 +1350,7 @@ client.on("message", function(message) {
              .setImage("https://i.imgur.com/QvBPSMc.gif")
              .setURL("https://youtu.be/rf_Go9eLaAM")
              .addField("Egg Bot Version 2.4 has been released!", "_ _")
-             .addField("- After a long two years, the long awaited version 1.7 of Egg Bot has been released to the public.", "_ _")
+             .addField("- After a long two years, the long awaited version 2.4 of Egg Bot has been released to the public.", "_ _")
              .addField("Change Log:", "_ _")
              .addField("- Added more references to drugs", "This was a necessary addition to Egg Bot as drugs do in fact make Egg Bot work more efficiently.")
              .addField("- Added Herobrine", "Removed Herobrine")
